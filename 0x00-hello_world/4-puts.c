a6e268adf222d5bf07bbb6f7145019308b81dd60
Michellebing <Michellegotau@gmail.com>
2023-06-15 16:45:29 -0700

    Michelle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include<studio.h>
+#include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Sucess)
+ * main - This program script print puts func
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}	
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
