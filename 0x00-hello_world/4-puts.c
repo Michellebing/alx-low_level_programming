a81e4d2255ba7025a168726b105da7fc46994c67
Michellebing <Michellegotau@gmail.com>
2023-06-15 14:27:12 -0700

    Joy

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * main - A program that print a line with puts function
+ * Return: 0 (Sucess)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
