55365b632f56cfdf5193c98da33ef8ad0f26a190
Michellebing <Michellegotau@gmail.com>
2023-06-17 13:16:42 -0700

    Porit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Enrty point
+ * Description: 'print alphabets in reverse'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 122;
+
+	while (c >= 97)
+	{
+		putchar(n);
+		n--;
+	}
+	putchar('\n');
+	return (0);
+}
