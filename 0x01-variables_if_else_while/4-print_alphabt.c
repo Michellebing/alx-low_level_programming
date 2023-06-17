347fc103dbd7a10caf7b9eef3e9f37e425fff798
Michellebing <Michellegotau@gmail.com>
2023-06-17 08:46:45 -0700

    Joy

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,14 +8,16 @@
  */
 int main(void)
 {
-	int i;
+	int n = 97;
 
-	for  (i 97; i < 123; i++)
+	while (n <= 122)
 	{
-		if (i != 101 && i != 113)
+		if (n == 101 || n == 113)
 		{
-			putchar(i);
+			n++;
+			continue;
 		}
+		putchar(n);
 	}
 	putchar('\n');
 	return (0);
