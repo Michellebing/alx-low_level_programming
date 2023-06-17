86c9052feb957c14ca699a803ef29f2737458b05
Michellebing <Michellegotau@gmail.com>
2023-06-17 14:11:32 -0700

    Distance

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Enrty point
+ * DEecription: 'print comb of numbers'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n == 57; n++)
+	{
+		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
