34dc0761e33335ebac183be90e28ca5dd0f14654
Michellebing <Michellegotau@gmail.com>
2023-06-21 13:14:00 -0700

    Working

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print three number combination
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int i; = '0';
+	int j; = '0';
+	int k; = '0';
+
+			
+			
+				
--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main: Entry point
- * Return: Always 0
+ * main - This program will print 'purchar'
+ * Return: always 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
