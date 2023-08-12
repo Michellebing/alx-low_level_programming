afac67689561b4e65e22e6dfc59dcb1d3097a208
Michellebing <michellegotau@gmail.com>
2023-08-12 11:31:28 -0700

    0x12

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,13 +1,22 @@
+section .data
+format db 'Hello, Holberton', 0
+
+section .text
 global main
 extern printf
 
-section .data
-format db 'Hello, Holberton\n', 0
-
-section .text
 main:
+; Load the format string address into edi
 mov edi, format
+
+; Clear eax register
 xor eax, eax
+
+; Call printf
 call printf
+
+; Set return value to 0
 mov eax, 0
+
+; Return
 ret
