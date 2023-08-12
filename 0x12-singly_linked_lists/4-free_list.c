38fdeb8f225b7cb9b3a51db459f70104fe41abdf
Michellebing <michellegotau@gmail.com>
2023-08-12 11:25:08 -0700

    0x12

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,22 @@
 #include <stdlib.h>
-#include "custom_lists.h"
+#include "lists.h"
 
 /**
- * free_custom_list - frees a custom linked list.
- * @head: custom list to be freed.
+ * free_list - A free linkedin list.
+ * @head: Pointer to the beginning of the list_t list to be freed.
  */
-void free_custom_list(custom_list_t *head)
+void free_list(list_t *head)
 {
-custom_list_t *temp;
+list_t *temp;
 
 while (head)
 {
 temp = head->next;
-free(head->data);
+
+free(head->str);
+
 free(head);
+
 head = temp;
 }
 }
